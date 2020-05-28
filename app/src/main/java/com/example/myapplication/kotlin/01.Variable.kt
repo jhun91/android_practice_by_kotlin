0287a76c0e83eb9913ceca49625dab4bc8b4c742
hun <fonercis@naver.com>
2020-05-28 22:31:02 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/kotlin/01.Variable.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication.kotlin
+
+//01. 변수
+
+var num = 10
+var hello = "hello"
+var point = 3.4
+
+val fix = 20
+
+fun main(args: Array<String>) {
+    println(num)
+    println(hello)
+    println(point)
+    println(fix)
+
+    num = 100
+    hello = "Good Bye"
+    point = 10.4
+
+    println(num)
+    println(hello)
+    println(point)
+    println(fix)
+}
