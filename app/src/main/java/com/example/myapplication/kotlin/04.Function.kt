45549310a3ba93ecf169f08c8084449083f7f3c6
hun <fonercis@naver.com>
2020-05-30 17:41:36 +0900

    함수 실습

---
+++ app/src/main/java/com/example/myapplication/kotlin/04.Function.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.kotlin
+
+// 04. Function
+
+// fun 함수명 (변수명: 타입 ...) : 반환형 {
+//      함수내용
+//      return 반환값
+// }
+
+fun plus(first: Int, second: Int): Int {
+    return first + second
+}
