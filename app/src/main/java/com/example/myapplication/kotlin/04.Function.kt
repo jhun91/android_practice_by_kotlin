5b0cbcf3dda273f2fd4c2cd19065c5fd6229cb6f
hun <fonercis@naver.com>
2020-05-31 12:42:34 +0900

    함수 실습

--- app/src/main/java/com/example/myapplication/kotlin/04.Function.kt
+++ app/src/main/java/com/example/myapplication/kotlin/04.Function.kt
@@ -11,6 +11,23 @@ fun plus(first: Int, second: Int): Int {
     return first + second
 }
 
+// -디폴트 값을 갖는 함수 만들기
+fun plusFive(first: Int, second: Int = 5): Int {
+    return first + second
+}
+
+// 간단하게 함수 만들기
+fun plusShort(first: Int, second: Int) = first + second
+
+// 가변인자를 갖는 함수
+fun plusMany(vararg numbers: Int) {
+    for (number in numbers) {
+        println(number)
+    }
+}
+
 fun main(args: Array<String>) {
     println(plus(2, 3))
+
+    plusMany(1,2,3,4,5,6,7,8)
 }
