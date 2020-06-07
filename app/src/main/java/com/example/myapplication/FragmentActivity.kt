1c9bc425ff9f71cad86d80cb0c767c0f276a38c5
Hun <fonercis@naver.com>
2020-06-07 15:25:00 +0900

    Fragment 실습

---
+++ app/src/main/java/com/example/myapplication/FragmentActivity.kt
@@ -0,0 +1,69 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.FragmentManager
+import kotlinx.android.synthetic.main.activity_fragment.*
+
+class FragmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
+        Log.d("life_cycle", "onCreate")
+
+        val fragmentOne: FragmentOne = FragmentOne()
+        // fragment에 데이터를 넣어주는 방법
+        val bundle: Bundle = Bundle()
+        bundle.putString("hello", "hello")
+        fragmentOne.arguments = bundle
+
+        button.setOnClickListener {
+            // fragment를 동적으로 작동하는 방법
+            // fragment 붙이는 방법 replace / add
+            val fragmentManager: FragmentManager = supportFragmentManager //AppCompatActivity가 가지고 있다.
+
+            // Transaction
+            // 작업의 단위 -> 시작과 끝이 있다.
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container, fragmentOne)
+            fragmentTransaction.commit()
+            // 끝을 내는 방법
+            // 1. commit    -> 시간 될 때 해 (좀더 안정적)
+            // 2. commitNow -> 지금 당장해
+        }
+
+        button2.setOnClickListener {
+            // fragment remove(떼고 붙이기 가능) / detach(한번 떼면 붙이기 불가) 하는 방법
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.detach(fragmentOne)
+            fragmentTransaction.commit()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("life_cycle", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("life_cycle", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("life_cycle", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("life_cycle", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("life_cycle", "onDestroy")
+    }
+}
